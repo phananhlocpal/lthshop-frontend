7857654cf31d52b3316eccc49940a4ddd2a9b7b0
Hughgz <minhhieu.swe@gmail.com>
2024-12-27 21:59:47 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,6 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 import ProviderLayout from "./Provider";
 import Zalo from "./Zalo";
 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -46,55 +45,52 @@ export default function RootLayout({ children }) {
           dangerouslySetInnerHTML={{
             __html: `
               window.__lc = window.__lc || {};
-              window.__lc.license = 18939557;
-              window.__lc.integration_name = "manual_onboarding";
-              window.__lc.product_name = "livechat";
-              (function (n, t, c) {
-                function i(n) {
-                  return e._h ? e._h.apply(null, n) : e._q.push(n);
-                }
-                var e = {
-                  _q: [],
-                  _h: null,
-                  _v: "2.0",
-                  on: function () {
-                    i(["on", c.call(arguments)]);
-                  },
-                  once: function () {
-                    i(["once", c.call(arguments)]);
-                  },
-                  off: function () {
-                    i(["off", c.call(arguments)]);
-                  },
-                  get: function () {
-                    if (!e._h)
-                      throw new Error(
-                        "[LiveChatWidget] You can't use getters before load."
-                      );
-                    return i(["get", c.call(arguments)]);
-                  },
-                  call: function () {
-                    i(["call", c.call(arguments)]);
-                  },
-                  init: function () {
-                    var n = t.createElement("script");
-                    (n.async = !0),
-                      (n.type = "text/javascript"),
-                      (n.src = "https://cdn.livechatinc.com/tracking.js"),
-                      t.head.appendChild(n);
-                  },
-                };
-                !n.__lc.asyncInit && e.init(),
-                  (n.LiveChatWidget = n.LiveChatWidget || e);
-              })(window, document, [].slice);
+          window.__lc.license = 18941855;
+          window.__lc.integration_name = "manual_onboarding";
+          window.__lc.product_name = "livechat";
+          (function (n, t, c) {
+            function i(n) {
+              return e._h ? e._h.apply(null, n) : e._q.push(n);
+            }
+            var e = {
+              _q: [],
+              _h: null,
+              _v: "2.0",
+              on: function () {
+                i(["on", c.call(arguments)]);
+              },
+              once: function () {
+                i(["once", c.call(arguments)]);
+              },
+              off: function () {
+                i(["off", c.call(arguments)]);
+              },
+              get: function () {
+                if (!e._h) throw new Error("[LiveChatWidget] You can't use getters before load.");
+                return i(["get", c.call(arguments)]);
+              },
+              call: function () {
+                i(["call", c.call(arguments)]);
+              },
+              init: function () {
+                var n = t.createElement("script");
+                n.async = !0;
+                n.type = "text/javascript";
+                n.src = "https://cdn.livechatinc.com/tracking.js";
+                t.head.appendChild(n);
+              },
+            };
+            !n.__lc.asyncInit && e.init();
+            n.LiveChatWidget = n.LiveChatWidget || e;
+          })(window, document, [].slice);
             `,
           }}
         />
         <noscript>
-          <a href="https://www.livechat.com/chat-with/18939557/" rel="nofollow">
+          <a href="https://www.livechat.com/chat-with/18941855/" rel="nofollow">
             Chat with us
           </a>
-          , powered by
+          , powered by{" "}
           <a
             href="https://www.livechat.com/?welcome"
             rel="noopener nofollow"
@@ -124,7 +120,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ProviderLayout>{children}</ProviderLayout>
-        <Zalo></Zalo> 
+        <Zalo></Zalo>
       </body>
       <GoogleAnalytics gaId="G-1HYQ8XTKYE" />
     </html>
